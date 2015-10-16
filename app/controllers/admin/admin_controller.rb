e818355788af2305bba61d48f6c6487324145518
Chalk <tao.cao@dianping.com>
2015-10-16 13:45:24 +0800

    add index page

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,10 @@
-class Admin::AdminController < ApplicationController
+class Admin::AdminController < ActionController::Base
   layout "admin"
   before_action :authenticate_user!
+  before_action :rend_side
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def rend_side
+      @side_label = Label.all
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :rend_side
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def rend_side
+      @side_label = Label.all
+      @recent_articles = Article.all.order(:created_at => :desc).limit(3)
+    end
 end
