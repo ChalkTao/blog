07b5288a22323dc76ed765158c06d0af101a7ef9
Chalk <tao.cao@dianping.com>
2015-11-03 03:45:11 +0800

    add nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/unicorn'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -14,19 +13,21 @@ require 'mina/unicorn'
 set :domain, 'qa'
 set :deploy_to, '/home/lead/rails'
 set :repository, 'https://github.com/ChalkTao/blog.git'
-set :branch, 'master'
+set :branch, 'nginx'
+set :keep_releases, 10
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'tmp/sockets', 'tmp/pids']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'log', 'tmp', 'public/assets', 'public/uploads']
 
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
+
+# Optional SSH settings:
+# SSH forward agent to ensure that credentials are passed through for git operations
 #   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -36,7 +37,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby-2.2.0@gemset]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -50,36 +51,32 @@ task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
 
-  queue %[
-    repo_host=`echo $repo | sed -e 's/.*@//g' -e 's/:.*//g'` &&
-    repo_port=`echo $repo | grep -o ':[0-9]*' | sed -e 's/://g'` &&
-    if [ -z "${repo_port}" ]; then repo_port=22; fi &&
-    ssh-keyscan -p $repo_port -H $repo_host >> ~/.ssh/known_hosts
-  ]
+  # Puma needs a place to store its pid file and socket file.
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/sockets"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/sockets"]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/pids"]
 end
+
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-  end
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-      invoke :'unicorn:restart'
+      # queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      # queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      # queue! %[kill -USR2 `cat #{deploy_to}/shared/pids/puma.pid`]
+      # invoke :'puma:phased_restart'
     end
   end
 end
@@ -90,3 +87,55 @@ end
 #  - http://nadarei.co/mina/tasks
 #  - http://nadarei.co/mina/settings
 #  - http://nadarei.co/mina/helpers
+
+namespace :puma do
+  set :web_server, :puma
+
+  set_default :puma_role,      -> { user }
+  set_default :puma_env,       -> { fetch(:rails_env, 'production') }
+  set_default :puma_config,    -> { "#{deploy_to}/#{shared_path}/config/puma.rb" }
+  set_default :puma_socket,    -> { "#{deploy_to}/#{shared_path}/tmp/sockets/puma.sock" }
+  set_default :puma_state,     -> { "#{deploy_to}/#{shared_path}/tmp/sockets/puma.state" }
+  set_default :puma_pid,       -> { "#{deploy_to}/#{shared_path}/tmp/pids/puma.pid" }
+  set_default :puma_cmd,       -> { "#{bundle_prefix} puma" }
+  set_default :pumactl_cmd,    -> { "#{bundle_prefix} pumactl" }
+  set_default :pumactl_socket, -> { "#{deploy_to}/#{shared_path}/tmp/sockets/pumactl.sock" }
+
+  # Make necessary direcotries exist
+  task :setup => :environment do
+    queue! "#touch {deploy_to}/#{shared_path}/tmp/sockets/"
+    queue! "#touch {deploy_to}/#{shared_path}/tmp/pids/"
+  end
+
+  desc 'Start puma'
+  task :start => :environment do
+    queue! %[
+      if [ -e '#{pumactl_socket}' ]; then
+        echo 'Puma is already running!';
+      else
+        if [ -e '#{puma_config}' ]; then
+          cd #{deploy_to}/#{current_path} && #{puma_cmd} -q -d -e #{puma_env} -C #{puma_config}
+        else
+          cd #{deploy_to}/#{current_path} && #{puma_cmd} -q -d -e #{puma_env} -b 'unix://#{puma_socket}' -S #{puma_state} --control 'unix://#{pumactl_socket}'
+        fi
+      fi
+    ]
+  end
+
+  desc 'Stop puma'
+  task stop: :environment do
+    queue! %[
+      if [ -e '#{pumactl_socket}' ]; then
+        cd #{deploy_to}/#{current_path} && #{pumactl_cmd} -S #{puma_state} stop
+      else
+        echo 'Puma is not running!';
+      fi
+    ]
+  end
+
+  desc 'Restart puma'
+  task restart: :environment do
+    invoke :'puma:stop'
+    invoke :'puma:start'
+  end
+end
