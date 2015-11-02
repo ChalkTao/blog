45d755d9e285b384bee098bdae23fade074ab38a
Chalk <tao.cao@dianping.com>
2015-11-03 04:00:01 +0800

    add nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,14 +31,14 @@ set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
-task :environment do
+# task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-2.2.0@gemset]'
-end
+#   invoke :'rvm:use[ruby-2.2.0@gemset]'
+# end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
 # For Rails apps, we'll make some of the shared paths that are shared between
