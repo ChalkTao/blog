3eec953c0dd5b1c17f5567a988c5ea97b1562378
Chalk <tao.cao@dianping.com>
2015-11-03 14:08:09 +0800

    fix nginx

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,8 @@ daemonize true
 threads 0,16
 workers 2
 
-deploy_to = '[deploy_dir]'
-shared_path = '[shared_folder]'
+deploy_to = '/home/lead/rails'
+shared_path = 'shared'
 bind       "unix://#{deploy_to}/#{shared_path}/tmp/sockets/puma.sock"
 state_path        "#{deploy_to}/#{shared_path}/tmp/sockets/puma.state"
 pidfile           "#{deploy_to}/#{shared_path}/tmp/pids/puma.pid"
